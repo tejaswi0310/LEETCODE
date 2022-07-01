9e10d89384dbd130a27d442c1f2d56e903966019
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-01 22:46:14 +0530

    Time: 168 ms (15.79%), Space: 30.5 MB (17.19%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
-    
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-//         vector<vector<int>> arr(intervals);
-        
-//         sort(arr.begin(),arr.end());
-        
-//         vector<int>res;
-        
-//         for(int i=0;i<intervals.size();i++){
-//             int ans=binary_search(arr.begin(),arr.end(),intervals[i][0]);//-arr.begin();
-//             res.push_back(ans);
-//         }
-        
-//         return res;
-        
         map<int,int>mp;
-        vector<int>res;
+        vector<int>v;
         
         for(int i=0;i<intervals.size();i++){
             mp[intervals[i][0]]=i;
@@ -24,14 +10,14 @@ public:
         
         for(auto it:intervals){
             auto ans=mp.lower_bound(it[1]);
+            
             if(ans==mp.end()){
-                res.push_back(-1);
+                v.push_back(-1);
             }
-            else{
-                res.push_back(ans->second);
-            }
+            else
+                v.push_back(ans->second);
         }
-        return res;
         
+        return v;
     }
 };
