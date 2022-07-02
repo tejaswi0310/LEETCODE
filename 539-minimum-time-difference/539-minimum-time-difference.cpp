8fee5d1feddf3b1957a21b6460c155fa5740b9fb
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-02 11:11:22 +0530

    Time: 29 ms (29.93%), Space: 13.8 MB (31.22%) - LeetHub

---
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    
+    int findMinDifference(vector<string>& time) {
+        vector<int>v;
+        
+        for(auto it:time){
+            int c=it[0]-'0';
+            int d=it[1]-'0';
+            int a=it[3]-'0';
+            int b=it[4]-'0';
+            
+            int h=c*10+d;
+            
+            // h=min(h,24-h);
+            int m=a*10+b;
+            v.push_back(h*60 + m);
+            
+        }
+        for(auto it:v){
+            cout<<it<<' ';
+        }
+        sort(v.begin(),v.end());
+        
+        int ans=INT_MAX;
+        
+        for(int i=1;i<v.size();i++){
+            ans=min(ans,v[i]-v[i-1]);
+        }
+        int n=v.size();
+        
+        ans=min(ans,v[0]-v[n-1]+60*24);
+        
+        return ans;
+    }
+};
