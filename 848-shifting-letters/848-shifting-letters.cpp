18796cb9d50226553eb16a11c10296000f63b19f
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-05 16:01:56 +0530

    Time: 276 ms (9.55%), Space: 67.7 MB (87.31%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cpp
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    string shiftingLetters(string s, vector<int>& shift) {
-        long long int shifts=0;
+    string shiftingLetters(string s, vector<int>& shifts) {
+        long shift=0;
         
-        for(int i=shift.size()-1;i>=0;i--){
-            s[i]=(s[i]-'a' +(shifts+shift[i])%26)%26+'a';
-            
-            shifts+=shift[i];
-        }
-        
-        return s;
+        for(int i=s.size()-1;i>=0;i--){
+            s[i]=(s[i]-'a'+(shift+shifts[i])%26)%26+'a';
+            shift+=shifts[i];
+        }return s;
     }
 };
