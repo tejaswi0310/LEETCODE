51e9cccf90a9337cfc8b65d5f13013446edd0e61
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-06 14:08:03 +0530

    Time: 128 ms (41.56%), Space: 28.3 MB (35.52%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        map<int,int>mp;
+        
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        
+        for(auto it:mp){
+            if(it.second){
+                int freq=it.second;
+                
+                for(int i=0;i<k;i++){
+                    if(mp[it.first+i]<freq){
+                        return false;
+                    }
+                    else{
+                        mp[it.first+i]-=freq;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
