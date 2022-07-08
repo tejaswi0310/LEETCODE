9718ea1e5f8ba8e8470cab0c9287c91baff19218
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-08 13:05:04 +0530

    Time: 1482 ms (10.89%), Space: 10.7 MB (31.52%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -4,13 +4,13 @@ public:
         unordered_map<int,int>mp;
         
         int ans=0;
-        int n=arr.size();
-        for(int i=0;i<n;i++){
+        
+        for(int i=0;i<arr.size();i++){
             ans=(ans+mp[target-arr[i]])%1000000007;
+            
             for(int j=0;j<i;j++){
                 mp[arr[i]+arr[j]]++;
             }
-        }
-        return ans;
+        }return ans;
     }
 };
