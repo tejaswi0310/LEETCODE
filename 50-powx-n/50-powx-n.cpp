8aecd2592bfda86c53490615b374dcfc5d8eca63
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-07-15 00:57:01 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (79.99%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -4,10 +4,11 @@ public:
         if(n==0){
             return 1;
         }
-        if(n<0){
-            return 1/x * myPow(1/x,-(n+1));
+        
+        else if(n<0){
+            return 1/x * (myPow(1/x,-(n+1)));
         }
         
-        return (n%2==0)? myPow(x*x,n/2): x*myPow(x*x,n/2);
+        return (n%2==0)? myPow(x*x,n/2): x* myPow(x*x,n/2);
     }
 };
