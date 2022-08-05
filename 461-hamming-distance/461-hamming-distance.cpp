f442921ef8160e6ef78c0af8f36198e09f53eda1
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-08-05 13:12:36 +0530

    Time: 4 ms (23.59%), Space: 6 MB (25.89%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int count=0;
-        
-        while(x or y){
+        while(x || y){
             count+=(x%2 != y%2);
             x>>=1;
             y>>=1;
         }
-        
         return count;
     }
 };
