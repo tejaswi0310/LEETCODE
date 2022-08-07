9214272f9d0a6de1728439c66b5de6857c095791
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-08-07 22:20:36 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (43.15%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+//     ++sVec[c1-'0']; ++gVec[c2-'0']; is used to count digits' (0~9) number which are not paired
+    // . bCnt += min(sVec[i], gVec[i]); for example, sVec[1] =2, gVec[1] =3, means there are 2 '1' in secret and 3 '1' in guess can not find paired one, then at least there are 2 '1' is in the wrong place.
+
+
     string getHint(string secret, string guess) {
         int count1=0;
         vector<int>first(10,0);
