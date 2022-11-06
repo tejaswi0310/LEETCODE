fc28df558aa0f230e2e44fdee2330004dec73b20
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2022-11-06 14:21:29 +0530

    Time: 402 ms (5.14%), Space: 59.5 MB (93.58%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        int left[n],right[n];
-        
-        left[0]=arr[0],right[n-1]=arr[n-1];
-        
-        for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],arr[i-1]);
+    int peakIndexInMountainArray(vector<int>& A) {
+        int l = 0, r = A.size() - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (A[mid] < A[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
         }
-        for(int i=n-2;i>=0;i--){
-            right[i]=max(right[i+1],arr[i+1]);
-        }
-        
-        for(int i=0;i<n;i++){
-            if(arr[i]>left[i] and arr[i]>right[i]){
-                return i;
-            }
-        }
-        return -1;
+        return l;
     }
 };
