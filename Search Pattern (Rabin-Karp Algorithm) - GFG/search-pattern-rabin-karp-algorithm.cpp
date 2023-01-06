30e94d0825c5f0d6c4b7941e7b8188325433fc5b
tejaswi0310 <66201742+tejaswi0310@users.noreply.github.com>
2023-01-06 20:51:15 +0530

    Added solution - LeetHub

---
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            vector<int>res;
+            for(int i=0;i<txt.size();i++){
+                if(txt.substr(i,pat.size())==pat){
+                    res.push_back(i+1);
+                }
+            }
+            if(res.size()==0){
+                return {-1};
+            }
+            return res;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
